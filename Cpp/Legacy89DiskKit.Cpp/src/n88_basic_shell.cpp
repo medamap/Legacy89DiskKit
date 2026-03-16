e6162cea43bde88fa34e86b0a6e7993c1e732470
MedamaP <junmera@gmail.com>
2026-03-16 18:47:51 +0900

    C++N88-BASICシェル実装を拡張

--- Cpp/Legacy89DiskKit.Cpp/src/n88_basic_shell.cpp
+++ Cpp/Legacy89DiskKit.Cpp/src/n88_basic_shell.cpp
@@ -1,6 +1,7 @@
 #include "legacy89diskkit/cpp/n88_basic_shell.hpp"
 
 #include "legacy89diskkit/cpp/hu_basic_name_rules.hpp"
+#include "legacy89diskkit/cpp/n88_basic_format_rules.hpp"
 
 namespace legacy89diskkit::cpp
 {
@@ -18,15 +19,83 @@ bool N88BasicShell::FileExists(
     const N88BasicConfiguration& config,
     const char* file_name)
 {
-    const auto files = ListFiles(directory_sectors, fat_data, config);
-    for (const auto& file : files)
-    {
-        if (HuBasicNameRules::BuildDisplayName(file.file_name, file.extension) == file_name)
-        {
-            return true;
-        }
-    }
+    return N88BasicFileLookup::Exists(directory_sectors, fat_data, config, file_name);
+}
 
-    return false;
+std::optional<N88BasicFileEntry> N88BasicShell::FindFile(
+    const std::vector<std::vector<std::uint8_t>>& directory_sectors,
+    const std::vector<std::uint8_t>& fat_data,
+    const N88BasicConfiguration& config,
+    const char* file_name)
+{
+    return N88BasicFileLookup::FindByName(directory_sectors, fat_data, config, file_name);
+}
+
+N88BasicFileSystemInfo N88BasicShell::GetFileSystemInfo(
+    const std::vector<std::uint8_t>& fat_data,
+    const N88BasicConfiguration& config)
+{
+    return N88BasicFileSystemInfoRules::BuildInfo(fat_data, config);
+}
+
+std::optional<N88BasicWriteTransactionPlan> N88BasicShell::PlanWrite(
+    const char* file_name,
+    const std::vector<std::uint8_t>& data,
+    const N88BasicFileAttributes& attributes,
+    const N88BasicConfiguration& config,
+    const std::vector<std::uint8_t>& fat_data)
+{
+    return N88BasicWriteTransaction::CreatePlan(file_name, data, attributes, config, fat_data);
+}
+
+std::optional<N88BasicDeleteTransactionPlan> N88BasicShell::PlanDelete(
+    const std::vector<std::uint8_t>& fat_data,
+    const std::vector<int>& clusters,
+    const std::vector<std::vector<std::uint8_t>>& directory_sectors,
+    const N88BasicConfiguration& config,
+    const char* file_name)
+{
+    return N88BasicDeleteTransaction::CreatePlan(
+        fat_data,
+        clusters,
+        directory_sectors,
+        config,
+        file_name);
+}
+
+std::optional<N88BasicRenameTransactionPlan> N88BasicShell::PlanRename(
+    const std::vector<std::vector<std::uint8_t>>& directory_sectors,
+    const N88BasicConfiguration& config,
+    const char* old_name,
+    const char* new_name)
+{
+    return N88BasicRenameTransaction::CreatePlan(
+        directory_sectors,
+        config,
+        old_name,
+        new_name);
+}
+
+std::optional<N88BasicAttributeUpdateTransactionPlan> N88BasicShell::PlanAttributeUpdate(
+    const std::vector<std::vector<std::uint8_t>>& directory_sectors,
+    const N88BasicConfiguration& config,
+    const char* file_name,
+    const N88BasicFileAttributes& attributes)
+{
+    return N88BasicAttributeUpdateTransaction::CreatePlan(
+        directory_sectors,
+        config,
+        file_name,
+        attributes);
+}
+
+std::vector<std::uint8_t> N88BasicShell::CreateFatData(const N88BasicConfiguration& config)
+{
+    return N88BasicFormatRules::CreateFatData(config);
+}
+
+std::vector<std::vector<std::uint8_t>> N88BasicShell::CreateDirectorySectors(const N88BasicConfiguration& config)
+{
+    return N88BasicFormatRules::CreateDirectorySectors(config);
 }
 }
