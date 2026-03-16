38da830434386bbddb6f09d07aab6ae07c06c870
MedamaP <junmera@gmail.com>
2026-03-16 16:47:50 +0900

    C++Hu-BASIC既定設定を拡張

--- Cpp/Legacy89DiskKit.Cpp/src/hu_basic_configuration.cpp
+++ Cpp/Legacy89DiskKit.Cpp/src/hu_basic_configuration.cpp
@@ -9,11 +9,11 @@ HuBasicConfiguration HuBasicConfigurationProvider::GetDefault(DiskType disk_type
     switch (disk_type)
     {
     case DiskType::TwoD:
-        return HuBasicConfiguration{ 2, 80, 16 * 256, 256 };
+        return HuBasicConfiguration{ 80, 16, 0, 15, 1, 1, 1, 16, 2, 80, 16 * 256, 256 };
     case DiskType::TwoDD:
-        return HuBasicConfiguration{ 2, 160, 16 * 256, 256 };
+        return HuBasicConfiguration{ 160, 16, 0, 15, 2, 1, 1, 16, 2, 160, 16 * 256, 256 };
     case DiskType::TwoHD:
-        return HuBasicConfiguration{ 3, 250, 16 * 256, 256 };
+        return HuBasicConfiguration{ 154, 26, 1, 3, 2, 1, 7, 20, 3, 250, 16 * 256, 256 };
     default:
         throw std::invalid_argument("Unsupported disk type for Hu-BASIC");
     }
